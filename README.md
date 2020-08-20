69e11ea145937ccf660082bc8d29792b7089653f
lokesh1199 <n0n4m399@tutanota.com>
2020-08-21 03:50:33 +0530

    Added all files

---
+++ README.md
@@ -0,0 +1,13 @@
+### Install
+```bash
+git clone https://github.com/lokesh1199/copy_for_CodeTantra
+cd copy_for_CodeTantra
+pip install -r requirements.txt
+```
+### Usage
+'''bash
+python main.py
+```
+
+### Files
+make sure you have the html file in current directory with name 'test.html'
---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+with open('test.html') as f:
+    soup = BeautifulSoup(f, 'html.parser')
+
+
+code = soup.find('div', class_='ace_layer ace_text-layer')
+
+code = code.find_all('div', class_='ace_line')
+
+with open('out.txt', 'w') as f:
+    for i in code:
+        f.write(i.text+'\n')
